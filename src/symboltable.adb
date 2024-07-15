5f1bd16d7320326aa893ddb2121cc62f358f0d2d
Sarah D. Furmanski <116507462+SarahDFur@users.noreply.github.com>
2024-07-15 23:47:52 +0300

    First draft of symbol table is complete

--- src/symboltable.adb
+++ src/symboltable.adb
@@ -4,43 +4,117 @@ with Utils; use Utils;
 with Ada.Directories; use Ada.Directories;
 with Ada.Characters.Conversions; use Ada.Characters.Conversions;
 with SymbolTable; use SymbolTable;
+with Utils; use Utils;
 
 --# File Mode Types:
 --    In_File: Opens an existing file for reading.
 --    Out_File: Creates a new file for writing (overwrites if the file already exists).
 --    Append_File: Opens an existing file for appending data to the end.
 
+--#  [ Name, Type, Kind, # ]
 package body SymbolTable is
    in_symbol_table_file, out_symbol_table_file : File_Type;  -- 2 variables for both operations
-   procedure Consturctor is
+   filename : Unbounded_String := To_Unbounded_String("");  -- Current files name - name of the class in our case
+   static_ids : Integer := 0;
+   field_ids : Integer := 0;
+   var_ids : Integer := 0;
+   arg_ids : Integer := 0;
+   --# Create a Symbol Table
+   procedure Consturctor (name: Unbounded_String) is
    begin
-      Create(File => out_symbol_table_file, Mode => Out_File, Name => Current_Directory & '\' & "symbol_table.txt");
+      Create(File => out_symbol_table_file, Mode => Out_File, Name => Current_Directory & '\' & To_String(name) & "_symbol_table.txt");
+      filename := To_String(name) & "_symbol_table.txt"; 
       Close(out_symbol_table_file);
    end Constructor;
 
-   procedure define (name: Unbounded_String; var_type: Unbounded_String; kind: Unbounded_String) is
+   procedure startSubroutine is  -- will be called when we close a jack / xml file
    begin
-      
+      -- reset constant vars for next xxxT.xml file that we read
+      static_ids := 0;
+      field_ids := 0;
+      var_ids := 0;
+      arg_ids := 0;
+   end startSubroutine;
+   
+   procedure define (name: Unbounded_String; var_type: Unbounded_String; kind: Unbounded_String) is
+      index : Integer := 0;
+      row : Unbounded_String := To_Unbounded_String("");
+   begin
+      Open(File => out_symbol_table_file, Mode => Append_File, Name => filename);
+      if To_String(kind) = "STATIC" then
+         index := static_ids;
+         static_ids := static_ids + 1;
+      elsif To_String(kind) = "FIELD" then
+         index := field_ids;
+         field_ids := field_ids + 1;
+      elsif To_String(kind) = "ARG" then
+         index := arg_ids;
+         arg_ids := arg_ids + 1;
+      elsif To_String(kind) = "VAR" then
+         index := var_ids; 
+         var_ids := var_ids + 1;
+      end if;
+      row := To_Unbounded_String(To_String(name) & " " & To_String(var_type) & " " & To_String(kind) & " " & index'Image);
+      Put_Line(File => out_symbol_table_file, Item => To_String(row));
+      Close(out_symbol_table_file);
    end define;
    
-   function varCount (kind: Unbounded_String) return int is  -- Might need to create global counters
+   function varCount (kind: Unbounded_String) return Integer is  -- Might need to create global counters
    begin
-      
+      if To_String(kind) = "STATIC" then
+         return static_ids;
+      elsif To_String(kind) = "FIELD" then
+         return field_ids;
+      elsif To_String(kind) = "ARG" then
+         return arg_ids;
+      elsif To_String(kind) = "VAR" then
+         return var_ids; 
+      end if;
    end varCount;
  
    function kindOf (name: Unbounded_String) return Unbounded_String is
+      kind_of : Unbounded_String := To_Unbounded_String("");
+      ins : String_Array;  
    begin
-      
+       Open(File => in_symbol_table_file, Mode => In_File, Name => To_String(filename));   
+      while not End_Of_File(in_symbol_file) and index_of = 0 loop
+         ins := Utils.split_string(Get_Line(File => in_symbol_table_file));
+         if To_String(ins(1)) = name then
+            kind_of := ins(3);
+         end if;
+      end loop;
+      Close(in_symbol_table_file);
+      return kind_of;
    end kindOf;
    
    function typeOf (name: Unbounded_String) return Unbounded_String is
+      type_of : Unbounded_String := To_Unbounded_String("");
+      ins : String_Array;  
    begin
-      
+       Open(File => in_symbol_table_file, Mode => In_File, Name => To_String(filename));   
+      while not End_Of_File(in_symbol_file) and index_of = 0 loop
+         ins := Utils.split_string(Get_Line(File => in_symbol_table_file));
+         if To_String(ins(1)) = name then
+            type_of := ins(2);
+         end if;
+      end loop;
+      Close(in_symbol_table_file);
+      return type_of;
    end typeOf;
    
-   function indexOf (name: Unbounded_String) return int is
+   function indexOf (name: Unbounded_String) return Integer is
+      index_of : Integer := 0;
+      ins : String_Array;
    begin
-      
+      Open(File => in_symbol_table_file, Mode => In_File, Name => To_String(filename));   
+      while not End_Of_File(in_symbol_file) and index_of = 0 loop
+         ins := Utils.split_string(Get_Line(File => in_symbol_table_file));
+         if To_String(ins(1)) = name then
+            index_of := Utils.string_to_int(To_String(ins(4)));
+         end if;
+      end loop;
+      Close(in_symbol_table_file);
+      return index_of;
    end indexOf;
 
 end SymbolTable;
--- src/symboltable.ads
+++ src/symboltable.ads
@@ -2,11 +2,14 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package SymbolTable is
 
-   procedure Consturctor;
+   type String_Array is array (Positive range <>) of Unbounded_String;
+   
+   procedure Consturctor (name: Unbounded_String);
+   procedure startSubroutine; 
    procedure define (name: Unbounded_String; var_type: Unbounded_String; kind: Unbounded_String);
-   function varCount (kind: Unbounded_String) return int;  -- Might need to create global counters
+   function varCount (kind: Unbounded_String) return Integer;  -- Might need to create global counters
    function kindOf (name: Unbounded_String) return Unbounded_String;
    function typeOf (name: Unbounded_String) return Unbounded_String;
-   function indexOf (name: Unbounded_String) return int;
+   function indexOf (name: Unbounded_String) return Integer;
 
 end SymbolTable;
